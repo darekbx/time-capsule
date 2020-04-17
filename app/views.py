5e0ba710627ba9aad3f463b3ef33b8fa1b8e1424
DarekMac <darek.baranczuk@gmail.com>
2020-04-17 16:38:35 +0200

    Fixes for files explorer

--- app/views.py
+++ app/views.py
@@ -5,15 +5,21 @@ from flask import g, render_template, redirect
 from flask_login import current_user, login_user, logout_user, login_required
 from dateutil.parser import parse
 
+from app.dir_utils import DirUtils
+
 import time, datetime as dt
 import os
 from os.path import expanduser
 import hashlib
 
 @app.route('/')
+@app.route('/dir/')
+@app.route('/dir/<path:dir>')
 @login_required
-def index():
-	return render_template('index.html')
+def index(dir = ""):
+	absolute_dir = app.config["RESOURCES-DIRECTORY"] + "/" + dir
+	content = DirUtils().fetch_content(absolute_dir, dir, dir is "")
+	return render_template('index.html', content = content)
 
 @login_manager.user_loader
 def load_user(user_id):
